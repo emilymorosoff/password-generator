cc565d74b53b46e016052da2d050737580132298
emilymorosoff <emilymorosoff@icloud.com>
2023-10-18 23:41:48 -0700

    Created functionality for adding lowercase letters if user accepted them.

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 let lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
 let specialCharacters = "!@#$%^&*()_+~`|}{[]:;?><,./-=".split("");
 let possibleCharacters = [];
+
 function generatePassword() {
   let numberOfCharacters = prompt("How many characters would you like in your password? Choose between 8-128 characters.")
   numberOfCharacters = parseInt (numberOfCharacters)
+  
   if (isNaN(numberOfCharacters) || numberOfCharacters<8 || numberOfCharacters>128) {
     return "Please choose valid number of characters."
   }
+  
   else {
     alert ("Your password will be " + numberOfCharacters + " characters long.")
   }
@@ -17,9 +20,16 @@ function generatePassword() {
   let includeUpperCaseLetters = confirm ("Would you like to include uppercase letters in your password?")
   let includeNumericValues = confirm ("Would you like to include numbers in your password?")
   let includeSpecialCharacters = confirm ("Would you like to include special characters in your password?")
+  
   if (!includeLowerCaseLetters && !includeUpperCaseLetters && !includeNumericValues && !includeSpecialCharacters) {
     return "Please select at least one character type."
   }
+  
+if (includeLowerCaseLetters) {
+possibleCharacters = possibleCharacters.concat (lowerCaseLetters)
+}
+console.log(possibleCharacters)
+
 }
 
 // Get references to the #generate element
